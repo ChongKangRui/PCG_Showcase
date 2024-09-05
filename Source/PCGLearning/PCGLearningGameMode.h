27886aa2f948cad7ddf2806b31cb4a56445a9b0e
ChongKangRui <chongkangrui@gmail.com>
2024-09-05 15:47:27 +0800

    Initial Pushed

---
+++ Source/PCGLearning/PCGLearningGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PCGLearningGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APCGLearningGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APCGLearningGameMode();
+};
+
+
+
